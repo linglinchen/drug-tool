be795e1f058e023f2f89f310b06cd95bfed248c0
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-02-25 10:15:38 -0600

    Begin building AtomComment endpoints

--- app/Http/Controllers/AtomCommentController.php
+++ app/Http/Controllers/AtomCommentController.php
@@ -7,13 +7,39 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Atom;
+use App\Comment;
+
 class AtomCommentController extends Controller
 {
     public function post($atomId) {
-        //
+        if(!Atom::findNewestIfNotDeleted($atomId)) {
+            //TODO: Atom not found
+        }
+
+        $comment = Comment::create([
+            'atomId' => $atomId,
+            'userId' => 1,		//TODO: make this use the user's actual id
+            'text' => $_POST['text']
+        ]);
+
+        return $comment;
     }
 
     public function delete($atomId, $commentId) {
-        //
+        if(!Atom::findNewestIfNotDeleted($atomId)) {
+            //TODO: Atom not found
+        }
+
+        $comment = Comment::where([
+				'id', '=', $commentId,
+				'atomId', '=', $atomId
+			]);
+
+        if(!$comment) {
+        	//TODO: comment not found
+        }
+
+        $comment->deleted(true)->save();
     }
 }
