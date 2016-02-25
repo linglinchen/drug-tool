23e92f54d6ba38faff5206cb9769a561d5758992
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-02-25 14:05:29 -0600

    Create a test for adding a comment

---
+++ tests/api/AddCommentCept.php
@@ -0,0 +1,14 @@
+<?php
+
+$I = new ApiTester($scenario);
+$I->wantTo('create a comment via the API');
+$I->sendPOST('/atom/1/comment', [
+	'atomId' => 1,
+	'parentId' => 0,
+	'text' => 'This comment was inserted by an automated test.'
+]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseMatchesJsonType([
+	'id' => 'integer'
+], '$');
