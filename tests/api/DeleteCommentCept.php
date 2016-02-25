3db05001399f20da090785acaa9af448378314ff
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-02-25 15:50:40 -0600

    Write test for comment deletion

---
+++ tests/api/DeleteCommentCept.php
@@ -0,0 +1,25 @@
+<?php
+
+$I = new ApiTester($scenario);
+$I->wantTo('delete a comment via the API');
+
+//create a comment to work on
+$I->sendPOST('/atom/1/comment', [
+	'atomId' => 1,
+	'parentId' => 0,
+	'text' => 'This comment was inserted by an automated test.'
+]);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseMatchesJsonType([
+	'id' => 'integer'
+], '$');
+$id = $I->grabDataFromResponseByJsonPath('$.id')[0];
+
+//now delete it
+$I->sendDELETE('/atom/1/comment/' . $id);
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+	'deleted' => true
+]);
