9ba4b02f4c551b722a39bd31ffa944e09e075221
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-08-03 15:25:18 -0500

    Document ApiError

--- app/ApiError.php
+++ app/ApiError.php
@@ -5,7 +5,18 @@ namespace App;
 use Illuminate\Http\Response;
 use App\ApiPayload;
 
+/**
+ * Provides a standardized way to create API error responses.
+ */
 class ApiError {
+	/**
+	 * Build an error response.
+	 *
+	 * @param int $code An HTTP error response code
+	 * @param string|string[]|null $msg (optional) The message(s) to return
+	 *
+	 * @return Response
+	 */
 	public static function buildResponse($code, $msg = null) {
 		$payload = new ApiPayload();
 		$payload->success = false;
