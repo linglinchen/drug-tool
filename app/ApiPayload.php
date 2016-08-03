3b4b7c5a3b8707cb7189289aec9f494372ba6eec
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-08-03 16:32:47 -0500

    Document ApiPayload

--- app/ApiPayload.php
+++ app/ApiPayload.php
@@ -2,15 +2,24 @@
 
 namespace App;
 
+/**
+ * A convenient, standardized way to build JSON API responses.
+ */
 class ApiPayload {
 	public $success = true;
 
+	/**
+	 * @param mixed $data (optional) Initialize the model with this data in the payload
+	 */
 	public function __construct($data = null) {
 		if($data !== null) {
 			$this->payload = $data;
 		}
 	}
 
+	/**
+	 * Return a JSON representation of this model whenever it is cast to a string.
+	 */
 	public function __toString() {
 		return json_encode($this);
 	}
