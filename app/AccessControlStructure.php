72b00565568b07368aa57023c96247b275a677b1
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-08-03 15:20:15 -0500

    Document AccessControlStructure

--- app/AccessControlStructure.php
+++ app/AccessControlStructure.php
@@ -6,11 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 use DB;
 
+/**
+ * Manages the ACL structure.
+ */
 class AccessControlStructure extends Model {
+	/**
+	 * @var string $table This model's corresponding database table
+	 */
 	protected $table = 'access_control_structure';
+
+	/**
+	 * @var string[] $guarded Columns that are protected from writes by other sources
+	 */
     protected $guarded = ['id'];
+
+	/**
+	 * @var string[] $dates The names of the date columns
+	 */
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
+    /**
+     * Load and initialize the ACL structure.
+     *
+     * @return array[] The ACL structure
+     */
 	public function getStructure() {
 		$output = array();
 		$structure = self::whereRaw('1 = 1')
