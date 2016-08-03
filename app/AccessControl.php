ff722c393923dfd24e430269fa129d2fb16e3896
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-08-03 15:20:12 -0500

    Document AccessControl

--- app/AccessControl.php
+++ app/AccessControl.php
@@ -9,11 +9,33 @@ use Log;
 
 use App\AccessControlStructure;
 
+/**
+ * Manages ACLs. Defaults to denial. Supports per-group and per-user permission settings. The behavior of this model
+ * is expected to match the ACL model in the UI.
+ */
 class AccessControl extends Model {
+	/**
+	 * @var string $table This model's corresponding database table
+	 */
 	protected $table = 'access_controls';
+
+	/**
+	 * @var string[] $guarded Columns that are protected from writes by other sources
+	 */
 	protected $guarded = ['id'];
+
+	/**
+	 * @var string[] $dates The names of the date columns
+	 */
 	protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
+	/**
+	 * Load the ACL into this model for the given user or the current user if none was provided.
+	 *
+	 * @param mixed[]|null $user (optional) Load permissions for this user
+	 *
+	 * @return array[] The user's ACL
+	 */
 	public function loadPermissions($user = null) {
 		//default to the current user if one isn't provided
 		if($user === null) {
@@ -31,6 +53,13 @@ class AccessControl extends Model {
 		return $this->permissions;
 	}
 
+	/**
+	 * Test if the loaded ACL tree permits the given action specified by the human-readable key.
+	 *
+	 * @param string $key The key of the permission to test
+	 *
+	 * @return boolean Can the user do the thing?
+	 */
 	public function can($key) {
 		if(!$this->permissions) {
 			$this->loadPermissions();
@@ -46,6 +75,14 @@ class AccessControl extends Model {
 		return false;
 	}
 
+	/**
+	 * Overlay the user's permissions onto the ACL structure to produce their ACL.
+	 *
+	 * @param string $structure array[] The ACL structure
+	 * @param string $permissions array[] The user's permissions
+	 *
+	 * @return array[] The ACL
+	 */
 	protected static function _applyPermissions($structure, $permissions) {
 		foreach($permissions as $permission) {
 			$structure[$permission['accessControlStructureId']]['permitted'] = (bool)$permission['permitted'];
@@ -54,6 +91,14 @@ class AccessControl extends Model {
 		return $structure;
 	}
 
+	/**
+	 * Recurse down the ACL tree to test if the user can do the thing.
+	 *
+	 * @param string $permission mixed[] The permission we are currently checking
+	 * @param string $permissions array[]|null (optional) The user's unvisited permissions
+	 *
+	 * @return boolean Can the user do the thing?
+	 */
 	protected function _resolvePermission($permission, $permissions = null) {
 		if($permissions === null) {
 			if(!$this->permissions) {
