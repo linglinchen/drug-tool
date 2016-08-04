d6c91e443aeb35d16699d1605d6e49f02d4eb949
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-08-04 09:28:50 -0500

    Document FuzzyRank

--- app/FuzzyRank.php
+++ app/FuzzyRank.php
@@ -2,7 +2,18 @@
 
 namespace App;
 
+/**
+ * Ranks search results based on their relevance to the search query.
+ */
 class FuzzyRank {
+	/**
+	 * Ranks search results based on their relevance to the search query.
+	 *
+	 * @param string[] $candidates The candidate strings to be ranked as key / value pairs (e.g. id => alphaTitle)
+	 * @param string $q The search query
+	 *
+	 * @return double[] The ranked candidates
+	 */
 	public static function rank($candidates, $q) {
 		$q = self::_prepareString($q);
 		$candidates = self::_prepareCandidates($candidates);
@@ -12,6 +23,14 @@ class FuzzyRank {
 		return $scores;
 	}
 
+	/**
+	 * Score candidate strings on their relevance to the search query.
+	 *
+	 * @param array[] $candidates The prepared candidate strings
+	 * @param string $q The prepared query
+	 *
+	 * @return double[] The scored candidates
+	 */
 	protected static function _scoreCandidates($candidates, $q) {
 		$scores = array();
 		foreach($candidates as $id => $candidate) {
@@ -21,6 +40,14 @@ class FuzzyRank {
 		return $scores;
 	}
 
+	/**
+	 * Score a candidate string on its relevance to the search query.
+	 *
+	 * @param string[] $candidate The prepared candidate string
+	 * @param string $q The prepared query
+	 *
+	 * @return double The candidate's score
+	 */
 	protected static function _scoreCandidate($candidate, $q) {
 		$magic = 42;
 		$delta = abs(sizeof($q) - sizeof($candidate));
@@ -48,6 +75,14 @@ class FuzzyRank {
 		return array_sum($score) / ($delta + 1);
 	}
 
+	/**
+	 * Prepare candidates for scoring and ranking.
+	 *
+	 * @param string[] $candidates The candidate strings to be ranked as key / value pairs (e.g. id => alphaTitle)
+	 * @param string $q The search query
+	 *
+	 * @return array[] The ranked candidates
+	 */
 	protected static function _prepareCandidates($candidates) {
 		foreach($candidates as $id => $candidate) {
 			$candidates[$id] = self::_prepareString($candidate);
@@ -56,12 +91,19 @@ class FuzzyRank {
 		return $candidates;
 	}
 
-	protected static function _prepareString($q) {
-		$q = strtolower($q);
-		$q = preg_replace('/[\W]/', ' ', $q);
-		$q = trim($q);
-		$q = preg_split('/\s+/', $q);
+	/**
+	 * Remove non-word characters, trim, and explode a string.
+	 *
+	 * @param string $input The string to prepare
+	 *
+	 * @return string[] The prepared string
+	 */
+	protected static function _prepareString($input) {
+		$output = strtolower($input);
+		$output = preg_replace('/[\W]/', ' ', $output);
+		$output = trim($output);
+		$output = preg_split('/\s+/', $output);
 
-		return $q;
+		return $output;
 	}
 }
