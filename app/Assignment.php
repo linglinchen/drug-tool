bf31651df6abe921fba352e1778fb4ef9bda0d56
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-08-09 14:09:54 -0500

    Sort assignment listing

--- app/Assignment.php
+++ app/Assignment.php
@@ -10,37 +10,34 @@ use App\Atom;
 
 class Assignment extends Model
 {
-    protected $table = 'assignments';
-    protected $guarded = ['id'];
-    protected $dates = ['created_at', 'updated_at'];
+	protected $table = 'assignments';
+	protected $guarded = ['id'];
+	protected $dates = ['created_at', 'updated_at'];
 
-    public static function getList($filters) {
-    	if($filters) {
-    		$output = self::where(DB::raw(1), '=', DB::raw(1));
+	public static function getList($filters) {
+		$output = self::orderBy('taskId');
 
-    		if(isset($filters['userId'])) {
-    			$output = $output->where('userId', '=', $filters['userId']);
-    		}
-    	}
-    	else {
-    		$output = self::all();
-    	}
-    	$output = $output->get()
-    			->toArray();
+		if($filters) {
+			if(isset($filters['userId'])) {
+				$output = $output->where('userId', '=', $filters['userId']);
+			}
+		}
+		$output = $output->get()
+				->toArray();
 
-    	$entityIds = array_column($output, 'atomEntityId');
-    	$atoms = Atom::findNewest($entityIds)
-    			->get()
-    			->toArray();
-    	foreach($output as &$row) {
-    		foreach($atoms as $atomKey => $atom) {
-	    		if($atom['entityId'] == $row['atomEntityId']) {
-	    			$row['atom'] = $atom;
-	    			unset($atoms[$atomKey]);
-	    		}
-	    	}
-    	}
+		$entityIds = array_column($output, 'atomEntityId');
+		$atoms = Atom::findNewest($entityIds)
+				->get()
+				->toArray();
+		foreach($output as &$row) {
+			foreach($atoms as $atomKey => $atom) {
+				if($atom['entityId'] == $row['atomEntityId']) {
+					$row['atom'] = $atom;
+					unset($atoms[$atomKey]);
+				}
+			}
+		}
 
-    	return $output;
-    }
+		return $output;
+	}
 }
