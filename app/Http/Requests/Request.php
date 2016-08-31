9894e27621faa9a4fd16116835f1c5cfc089eea7
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-08-30 20:30:49 -0500

    Allow camelCase post and get keys

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -22,7 +22,12 @@ class Request extends \Illuminate\Http\Request {
 	 * @return string|array
 	 */
 	public function input($key = null, $default = null) {
-		$key = $key === null ? null : snake_case($key);
+		if($key === null) {
+			$key = null;
+		}
+		else if(!parent::input($key)) {
+			$key = snake_case($key);
+		}
 
 		return AppModel::arrayKeysToSnakeCase(parent::input($key, $default));
 	}
