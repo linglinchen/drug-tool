2e72bb9eb2e0cad7b00cf98e3d481476dd5417c3
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-11-14 09:52:48 -0600

    Add some stub methods

--- app/Providers/ApiUserProvider.php
+++ app/Providers/ApiUserProvider.php
@@ -7,4 +7,41 @@ use Illuminate\Support\ServiceProvider;
 
 use App\UserProduct;
 
-class ApiUserProvider extends EloquentUserProvider {}
+class ApiUserProvider extends EloquentUserProvider {
+    /**
+     * Retrieve a user by their unique identifier.
+     *
+     * @param  mixed  $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier) {
+        $user = parent::retrieveById($identifier);
+
+        return $user;
+    }
+
+    /**
+     * Retrieve a user by their unique identifier and "remember me" token.
+     *
+     * @param  mixed  $identifier
+     * @param  string  $token
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByToken($identifier, $token) {
+        $user = parent::retrieveByToken($identifier, $token);
+
+        return $user;
+    }
+
+    /**
+     * Retrieve a user by the given credentials.
+     *
+     * @param  array  $credentials
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByCredentials(array $credentials) {
+        $user = parent::retrieveByCredentials($credentials);
+
+        return $user;
+    }
+}
