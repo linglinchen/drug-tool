41b9c8725e5ca9aeeedf8c8975a38463accdceb5
Russell, Craig (ELS-CON) <c.russell.2@elsevier.com>
2016-11-23 13:19:38 -0600

    Fix key's value in migration

--- database/migrations/2016_10_10_125100_create_products_table.php
+++ database/migrations/2016_10_10_125100_create_products_table.php
@@ -20,7 +20,7 @@ class CreateProductsTable extends Migration
         });
 
         DB::table('products')->insert([
-            'key' => 'NDR',
+            'key' => 'ndr',
             'title' => 'Skidmore NDR',
             'doctype' => 'drug'
         ]);
