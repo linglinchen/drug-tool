aad3a41d513e3399af5528d387a4878c0d1a1581
Ling Lin <LIN4@science.regn.net>
2017-01-31 09:52:45 -0600

    inserted domain to access_control_structure

---
+++ database/migrations/2017_01_30_214841_create_domains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('title');
+            $table->timestamps();
+
+            $table->index('code');
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('domains');
+    }
+}
+
