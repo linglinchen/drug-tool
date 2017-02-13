03d95d53016e328211b775e8e09212ffa56f30a6
Ling Lin <LIN4@science.regn.net>
2017-02-13 16:33:58 -0600

    fixed saving new atom for vet

--- app/DictionaryDoctype.php
+++ app/DictionaryDoctype.php
@@ -20,16 +20,18 @@ class DictionaryDoctype extends AbstractDoctype {
     ];
 
     public function beforeSave($atom) {
-        $originalAtom = Atom::findNewestIfNotDeleted($atom->entity_id, $atom->product_id);
-        $originalDomainCode = $originalAtom->domain_code;
-        if($originalDomainCode != $atom->domain_code) {
-            $replacement = '$1' . $atom->domain_code . '$3';
-            $atom->xml = preg_replace('/(<category[^>]*>)(.*)(<\/category>)/Ssi', $replacement, $atom->xml);
-        }
-        else { 
-            preg_match('/<category[^>]*>(.*)<\/category>/Si', $atom->xml, $matches);
-            if($matches) {
-                $atom->domain_code = trim($matches[1]);
+         $originalAtom = Atom::findNewestIfNotDeleted($atom->entity_id, $atom->product_id);
+         if ($originalAtom){ // for existing atom, if it's new atom, just return true
+            $originalDomainCode = $originalAtom->domain_code;
+            if($originalDomainCode != $atom->domain_code) {
+                $replacement = '$1' . $atom->domain_code . '$3';
+                $atom->xml = preg_replace('/(<category[^>]*>)(.*)(<\/category>)/Ssi', $replacement, $atom->xml);
+            }
+            else {
+                preg_match('/<category[^>]*>(.*)<\/category>/Si', $atom->xml, $matches);
+                if($matches) {
+                    $atom->domain_code = trim($matches[1]);
+                }
             }
         }
         return true;
