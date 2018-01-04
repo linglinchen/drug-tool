d8f5d8117395463b55b8c97b4ce55ce0d1ce61df
Ling Lin <LIN4@science.regn.net>
2018-01-04 11:31:29 -0600

    clean up, but run slow

--- app/Console/Commands/QuickFixVetXref.php
+++ app/Console/Commands/QuickFixVetXref.php
@@ -1,6 +1,6 @@
 <?php
 
-/* search for the vet terms that xref needed (it's missing from recent transformation)
+/* search for the vet terms that xref is needed based on previous transformation (it's missing from recent transformation)
  * insert <xref> to them
 */
 
@@ -33,20 +33,72 @@ class QuickFixVetXref extends Command {
      * @return mixed
      */
     public function handle() {
+        ini_set('memory_limit', '1280M');
        $xrefLocation = self::_getXrefBasedOnOldXml();
        $entityIdArray = self::_getEntityIdArray();
 
+       //insert <xref> into the xml
+        $atoms = Atom::whereIn('id', function ($q) {
+                    Atom::buildLatestIDQuery(null, $q);
+                })->where('product_id','=', 3)->get();
+
+        foreach($atoms as $atom) {
+            foreach ($xrefLocation as $xref){
+                foreach ($xref as $term => $ref){
+                    if ($atom->alpha_title == $term && $atom->alpha_title == 'A-mode'){ //if atom has xref
+                        $newAtom = $atom->replicate();
+                        $newXml = $atom->xml;
+                        foreach ($ref as $refered){
+                            if (array_key_exists($refered, $entityIdArray)){
+                                $entityId = $entityIdArray[$refered];
+                                echo "$term\t$refered\t$entityId\n";
+                                $referedPattern = '/(<xref refid="tra__REFID__">)?'.$refered.'(</xref>)?/';
+                                $replacement = '<xref refid="a:'.$entityId.'">'.$refered.'</xref>';
+                                $newXml = preg_replace($referedPattern, $replacement, $newXml);
+                            }
+                        }
+                        $newAtom->xml = $newXml;
+                        $newAtom->modified_by = null;
+						$newAtom->save();
+                        exit;
+                    }
+                }
+            }
+        }
+
+        // $xml = '<entry sortorder="758" type="shared">
+		// 		<headw id="hw_REPLACE_ME__">hepatoprotectant</headw>
+		// 		<category cat_id="cat_REPLACE_ME__">PHTHG</category>
+		// 		<defgroup type="shared">
+		// 			<def id="d_REPLACE_ME__" n="1">compounds that may provide the liver with some protection from toxins or biochemical injury. Includes ursodeoxycholic acid, <emphasis style="italic">S</emphasis>-adenosyl-<emphasis style="smallcaps">l</emphasis>- methionine, and silymarin.    Many are nutraceuticals with uncertain efficacy.</def>
+		// 		</defgroup>
+		// 	</entry>';
+        // $term = 'hepatoprotectant';
+        // $ref=['silymarin', 'S-adenosyl-l- methionine'];
+        // $newXml = $xml;
+        // foreach ($ref as $refered){
+        //     if (array_key_exists($refered, $entityIdArray)){
+        //         $entityId = $entityIdArray[$refered];
+        //         echo "$term\t$refered\t$entityId\n";
+        //         $referedPattern = '/(<xref refid="tra__REFID__">)?'.$refered.'(<\/xref>)?/';
+        //         $replacement = '<xref refid="a:'.$entityId.'">'.$refered.'</xref>';
+        //         $newXml = preg_replace($referedPattern, $replacement, $newXml);
+        //         echo "$newXml\n";
+        //     }
+        // }
     }
 
     public static function _getEntityIdArray() {
+        $entityIdArray = [];
         $atoms = Atom::whereIn('id', function ($q) {
-                    Atom::buildLatestIDQuery(null, $q);
-                })->get();
+                        Atom::buildLatestIDQuery(null, $q);
+                    })->where('product_id','=', 3)->get();
 
         foreach($atoms as $atom) {
-            echo "$atom->alpha_title\t$atom->entity_id\n";
+            $entityIdArray[$atom->alpha_title] = $atom->entity_id;
+            //echo "$atom->alpha_title\t$atom->entity_id\n";
         }
-        return true;
+        return $entityIdArray;
     }
 
     public static function _getXrefBasedOnOldXml() {
@@ -55,53 +107,57 @@ class QuickFixVetXref extends Command {
         $files = array_slice($files, 2);
         $xref_arr = [];
         foreach ($files as $file){
-            //$file = 'sarcoptic_mange.xml';
+            //if ($file == 'letter_a_4e.xml'){
             $xml = file_get_contents($dir.'/'.$file);
             $doc = new \DOMDocument();
             $doc->loadXML($xml);
             $xpath = new \DOMXpath($doc);
-            $chapterElements = $xpath->query('//entry');
+            $chapterElements = $xpath->query('/dictionary/body/alpha/entry');
             foreach($chapterElements as $chapterElement) {
+                $headw = '';
+                $ref_array = [];
+                $ref_unique = [];
                 foreach($chapterElement->childNodes as $atomNode) {
-                    $headw = '';
-                    $ref_array = [];
-                    if(!isset($atomNode->tagName)) {
-                        continue;
-                    }
-
-                    $xml = $doc->saveXML($atomNode);
-                    /* preg_match_all('/<xref refid="tra__REFID__"( id_legacy="[\d]+")?>(.*)<\/xref>/Si', $xml, $matches); */
-                    preg_match_all('/<xref refid="tra__REFID__">([^<]*)<\/xref>/Si', $xml, $matches);
-                    if ($matches[1]){
-                        foreach($matches[1] as $match){
-                            array_push($ref_array, $match);
-                        }
-                        foreach($atomNode->parentNode->childNodes as $node){
-                            if (isset($node->tagName) && $node->tagName == 'headw'){
-                                //$headw = $node->textContent;
-                                $headw = trim($node->nodeValue);
+                    if(isset($atomNode->tagName) && ($atomNode->tagName == 'headw')) {
+                        $headw = trim(str_replace("\n", '', $atomNode->nodeValue));
+                    }else{
+                        $xml = $doc->saveXML($atomNode);
+                        if (strlen(trim($xml)) > 0 && strlen($headw) > 0){
+                            $termDoc = new \DOMDocument();
+                            $termDoc->loadXML($xml);
+                            $termXpath = new \DOMXpath($termDoc);
+                            $xrefElements = $termXpath->query('//xref');
+                            foreach ($xrefElements as $xrefElement){
+                                $xrefText = trim(str_replace("\n", '',$xrefElement->textContent));
+                                if (strlen($xrefText)>0){
+                                    array_push($ref_array, $xrefText);
+                                }else{
+                                    //echo "self closing xref: $headw\n";
+                                }
                             }
-                        }
-
-                        if ($headw){
-                            $ref_uniq = array_unique($ref_array);
-                            array_push($xref_arr, array($headw => $ref_uniq));
                         }
                     }
                 }
+                $ref_uniq = array_unique($ref_array);
+                if ($ref_uniq){
+                    array_push($xref_arr, array($headw => $ref_uniq));
+                }
+                    /* preg_match_all('/<xref refid="tra__REFID__"( id_legacy="[\d]+")?>(.*)<\/xref>/Si', $xml, $matches);
+                     preg_match_all('/<xref refid="tra__REFID__">([\d\D]*)<\/xref>/Si', $xml, $matches);*/
             }
+            //}
         }
         $xref_arr = array_unique($xref_arr, SORT_REGULAR);
-
         echo "term\trefered\n";
         foreach ($xref_arr as $xref){
             foreach ($xref as $term => $ref){
-                foreach ($ref as $refered){
-                    //echo "$term\t$refered\n";
-                }
+                //if ($term == 'hepatoprotectant'){
+                    foreach ($ref as $refered){
+                        //echo "$term\t$refered\n";
+                    }
+                //}
             }
         }
-
         return $xref_arr;
     }
 }
