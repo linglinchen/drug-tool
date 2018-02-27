ea317b2efafcce5e5dcf87498bc993979b8fcd88
Judith Zettle <j.zettle@elsevier.com>
2018-02-27 16:03:43 -0600

    consolidating if statements

--- app/Assignment.php
+++ app/Assignment.php
@@ -54,8 +54,6 @@ class Assignment extends AppModel {
 			Comment::addSummaries($atoms, $productId);
 			foreach ($atoms as $atom){
 				$atom->addDomains($productId);
-			}
-			foreach ($atoms as $atom){
 				$atom->addCommentSuggestions($atom['entity_id']);
 			}
 			$atoms = $atoms->toArray();
