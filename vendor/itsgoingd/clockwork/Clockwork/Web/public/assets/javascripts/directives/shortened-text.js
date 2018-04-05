d4b295d59dca7ea1cd243835ee8ec51c206664b1
Zettle, Judy (ELS-STL) <j.zettle@elsevier.com>
2018-04-05 20:36:02 +0000

    Revert "reinstate vendor directory"
    This reverts commit a87905c30f84a1089d5365a3267ed6829d1a2a73

--- vendor/itsgoingd/clockwork/Clockwork/Web/public/assets/javascripts/directives/shortened-text.js
+++
@@ -1,9 +0,0 @@
-Clockwork.directive('shortenedText', function () {
-	return {
-		link: function (scope, element, attrs) {
-			$(element).on('click', function () {
-				$(this).html($(this).attr('shortened-text'))
-			})
-		}
-	}
-})
