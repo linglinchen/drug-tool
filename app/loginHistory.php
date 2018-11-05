022700eca7b7c295f8f9e53786e090484bc9c11f
T Costello <t.costello@elsevier.com>
2018-11-05 12:13:43 -0600

    add logging logins

---
+++ app/loginHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use App\AppModel;
+
+class LoginHistory extends AppModel {
+		public $timestamps = false;
+    protected $table = 'login_history';
+    protected $guarded = ['id'];
+    protected $dates = ['login_time'];
+}
