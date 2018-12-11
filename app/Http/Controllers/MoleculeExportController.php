0020c4171dc1a862b14553dcd716c078c564daa5
Jonathan Schmidt <j.schmidt@elsevier.com>
2018-12-10 18:54:33 -0600

    going home tonight, need to work on addIllustrationLog

--- app/Http/Controllers/MoleculeExportController.php
+++ app/Http/Controllers/MoleculeExportController.php
@@ -33,13 +33,23 @@ class MoleculeExportController extends Controller {
      * @return ApiPayload|Response
      */
     public function getAction($productId, $code, Request $request) {
-        $statusId = $request->input('statusId');
+		//TODO: build method to fetch product info from db
+		$productInfo = [
+			'isbn' => '0000000000000',
+			'author' => '',
+			'title' => '',
+			'edition' => 0,
+			'cds' => [
+				'firstname' => '',
+				'lastname' => '',
+				'phone' => '1-000-000-0000',
+				'email' => '',
+			],
+		];
+		$statusId = $request->input('statusId');
         $statusId = $statusId === '' ? null : (int)$statusId;
         $withFigures = $request->input('withFigures');
         $withFigures = $withFigures === '' ? null : $withFigures;
-        //TODO: pick these up from configuration
-        $s3UrlDev = 'https://s3.amazonaws.com/metis-imageserver-dev.elseviermultimedia.us';
-        $s3UrlProd = 'https://s3.amazonaws.com/metis-imageserver.elseviermultimedia.us';
 
         //method in Product model to find doctype returns a whole object, so this is extra code to deduce the doctype from the name of the class of the object returned.
         $doctype = 'drug';
@@ -66,90 +76,131 @@ class MoleculeExportController extends Controller {
         $filename = $code . '_xml_'. $zipDate .'.zip';
         $filepath = tempnam('tmp', $code . '_xml_zip');     //generate the zip in the tmp dir, so it doesn't hang around
         $result = $zip->open($filepath, \ZipArchive::OVERWRITE);
-        //Top Header material for tab delimited illustration log download. static content added to log.
-$metaheader_vet = <<<METAHEADER
+
+		$moleculeXml = $molecule->export($statusId);
+
+		//If doctype is dictionary, different xml wrapper is written.
+		if($doctype === 'dictionary') {
+			$xmlDoctype = '<!DOCTYPE dictionary PUBLIC "-//ES//DTD dictionary DTD version 1.0//EN//XML" "https://major-tool-development.s3.amazonaws.com/DTDs/Dictionary_4_5.dtd">';
+			$xmlRoot = 'dictionary';
+
+			switch ((int)$productId) { //TODO: make the ISBNs dynamic
+				case 3:
+					$productInfo = [
+						'isbn' => '9780702074639',
+						'author' => 'Saunders',
+						'title' => 'Veterinary Dictionary',
+						'edition' => 5,
+						'cds' => [
+							'firstname' => 'Sarah',
+							'lastname' => 'Vora',
+							'phone' => '1 314 447 8326',
+							'email' => 'sa.vora@elsevier.com',
+						],
+					];
+
+					break;
+				case 5:
+					$productInfo = [
+						'isbn' => '9780323546355',
+						'author' => 'Mosby',
+						'title' => 'Dental Dictionary',
+						'edition' => 4,
+						'cds' => [
+							'firstname' => 'Sarah',
+							'lastname' => 'Vora',
+							'phone' => '1 314 447 8326',
+							'email' => 'sa.vora@elsevier.com',
+						],
+					];
+
+					break;
+				default:
+					//nothing special to do here
+					break;
+			}
+
+			$xml = $xmlDoctype . "\n";
+			$xml .= '<' . $xmlRoot . ' isbn="' . $productInfo['isbn'] . '">' . "\n";
+			$xml .= '<body>' . "\n";
+			$xml .= $moleculeXml;
+			$xml .= '</body>' . "\n";
+			$xml .= '</'. $xmlRoot . '>';
+			$zip->addFromString($code . '.xml', $xml);
+
+			//Top Header material for tab delimited illustration log download. static content added to log.
+			$metaheader_default = <<<METAHEADER
 Illustration Processing Control Sheet\t\t\t\t\t\t\t\t\t\t\t
-Author:\tSaunders\t\t\t\t\t\t\t\t\t\t\tISBN:\t9780702074639\t\t\t\t
-Title:\tVet Dictionary\t\t\t\t\t\t\t\t\t\t\tEdition:\t5\t\t\t\t
-Processor:\tErin Garner\t\t\t\t\t\t\t\t\t\t\tChapter:\t{$code}\t\t\t\t
-Phone/Email:\t314 447 8971/e.garner@elsevier.com\t\t\t\t\t\t\t\t\t\t\tDate:\t{$zipDate}\t\t\t\t
+Author:\t{$productInfo['author']}\t\t\t\t\t\t\t\t\t\t\tISBN:\t{$productInfo['isbn']}\t\t\t\t
+Title:\t{$productInfo['title']}\t\t\t\t\t\t\t\t\t\t\tEdition:\t{$productInfo['edition']}\t\t\t\t
+Processor:\t{$productInfo['cds']['firstname']} {$productInfo['cds']['lastname']}\t\t\t\t\t\t\t\t\t\t\tChapter:\t{$code}\t\t\t\t
+Phone/Email:\t{$productInfo['cds']['phone']}/{$productInfo['cds']['email']}\t\t\t\t\t\t\t\t\t\t\tDate:\t{$zipDate}\t\t\t\t
 Figure Number\tPieces (No.)\tDigital (Y/N)\tTo Come\t Previous edition fig #\t Borrowed from other Elsevier sources (author(s), title, ed, fig #)\tDigital file name (include disc number if multiple discs)\tFINAL FIG FILE NAME\t 1/C HT\t 2/C HT\t 4/C HT\t 1/C LD\t 2/C LD\t 4/C LD\tArt category\tArt point of contact\t Comments\n
 METAHEADER;
-$metaheader_dental = <<<METAHEADER
-Illustration Processing Control Sheet\t\t\t\t\t\t\t\t\t\t\t
-Author:\Mosby\t\t\t\t\t\t\t\t\t\t\tISBN:\t9780323546355\t\t\t\t
-Title:\tDental Dictionary\t\t\t\t\t\t\t\t\t\t\tEdition:\t4\t\t\t\t
-Processor:\tSarah Vora\t\t\t\t\t\t\t\t\t\t\tChapter:\t{$code}\t\t\t\t
-Phone/Email:\t314 447 8326/sa.vora@elsevier.com\t\t\t\t\t\t\t\t\t\t\tDate:\t{$zipDate}\t\t\t\t
-Figure Number\tPieces (No.)\tDigital (Y/N)\tTo Come\t Previous edition fig #\t Borrowed from other Elsevier sources (author(s), title, ed, fig #)\tDigital file name (include disc number if multiple discs)\tFINAL FIG FILE NAME\t 1/C HT\t 2/C HT\t 4/C HT\t 1/C LD\t 2/C LD\t 4/C LD\tArt category\tArt point of contact\t Comments\n
-METAHEADER;
 
-        //If doctype is dictionary, different xml wrapper is written.
-        if($doctype === 'dictionary') {
-            $moleculeXml = $molecule->export($statusId);
-            switch ((int)$productId) { //TODO: make the ISBNs dynamic
-                case 3:
-                    $xml = '<!DOCTYPE dictionary PUBLIC "-//ES//DTD dictionary DTD version 1.0//EN//XML" "https://major-tool-development.s3.amazonaws.com/DTDs/Dictionary_4_5.dtd">' . "\n";
-                    $xml .= '<dictionary isbn="9780702074639">' . "\n"; //vet edition 5           vet 4 is 9780702032318
-                    $xml .= '<body>' . "\n";
-                    $xml .= $moleculeXml;
-                    $xml .= '</body>' . "\n";
-                    $xml .= '</dictionary>';
-                    $figureLog =  $molecule->addFigureLog($moleculeXml, $metaheader_vet);
-                    $zip->addFromString($code . '.xml', $xml);
-                    $zip->addFromString('IllustrationLog_' . $code . '.tsv' ,  $figureLog);
-                    $zip->close();
-                    break;
-                case 5:
-                    $xml = '<!DOCTYPE dictionary PUBLIC "-//ES//DTD dictionary DTD version 1.0//EN//XML" "https://major-tool-development.s3.amazonaws.com/DTDs/Dictionary_4_5.dtd">' . "\n";
-                    $xml .= '<dictionary isbn="9780323546355">' . "\n"; //dental edition 4          dental 3 is 9780323100120
-                    $xml .= '<body>' . "\n";
-                    $xml .= $moleculeXml;
-                    $xml .= '</body>' . "\n";
-                    $xml .= '</dictionary>';
-                    $figureLog = $molecule->addFigureLog($moleculeXml, $metaheader_dental);
-                    $zip->addFromString($code . '.xml', $xml);
-                    $zip->addFromString('IllustrationLog_' . $code . '.tsv' ,  $figureLog);
-                    $imageFiles = $molecule->getImageFileName($moleculeXml);
-                    foreach ($imageFiles as $imageFile){
-                        if (substr($imageFile, 0, 9) == 'suggested'){ //suggested image
-                            $fileName1 = $s3UrlProd."/".$imageFile.".jpg";
-                            if (@file_get_contents($fileName1)){        //FIXME: Error suppression is a bad practice
-                                $zip->addFromString($imageFile.'.jpg', file_get_contents($fileName1));
-                            }
-                            $fileName2 = $s3UrlProd."/".$imageFile.".JPG";
-                            if (@file_get_contents($fileName2)){        //FIXME: Error suppression is a bad practice
-                                $zip->addFromString($imageFile.'.JPG', file_get_contents($fileName2));
-                            }
-                        }
-                        else { //legacy image
-                            $zip->addFromString($imageFile.'.jpg', file_get_contents($s3UrlProd."/9780323100120/".$imageFile.".jpg"));	//TODO: make the ISBN dynamic
-                        }
-                    }
+			$figureLog = $molecule->addFigureLog($moleculeXml, $metaheader_default);
+			$zip->addFromString('IllustrationLog_' . $code . '.tsv' ,  $figureLog);
+			
+			$this->addIllustrations($moleculeXml, $zip, $productInfo);
 
-                    $zip->close();
-                    break;
+		//If doctype is questions, different xml wrapper is written.
+		} elseif($doctype === 'questions') {
+			$xmlDoctype = '<!DOCTYPE questions PUBLIC "-//ES//DTD questions DTD version 1.1//EN//XML" "https://major-tool-development.s3.amazonaws.com/DTDs/questions_1_1.dtd">';
+			$xmlRoot = 'questions';
+			
+			switch ((int)$productId) { //TODO: make the ISBNs dynamic
+				case 7:
+					$productInfo = [
+						'isbn' => '9780702074639',
+						'author' => 'Silvestri',
+						'title' => 'Saunders 2019-2020 Strategies for Test Success',
+						'edition' => 6,
+						'cds' => [
+							'firstname' => 'Laura',
+							'lastname' => 'Goodrich',
+							'phone' => '1 314 447 8538',
+							'email' => 'l.goodrich@elsevier.com',
+						],
+					];
 
-                default:
-                    $xml = '<!DOCTYPE dictionary PUBLIC "-//ES//DTD dictionary DTD version 1.0//EN//XML" "https://major-tool-development.s3.amazonaws.com/DTDs/Dictionary_4_5.dtd">' . "\n";
-                    $xml .= '<dictionary isbn="0000000000000">' . "\n";	//TODO: make the ISBN dynamic
-                    $xml .= '<body>' . "\n";
-                    $xml .= $molecule->export($statusId);
-                    $xml .= '</body>' . "\n";
-                    $xml .= '</dictionary>';
-                    $zip->addFromString($code . '.xml', $xml);
-                    $zip->close();
-                    break;
-            }
-        }
-        else {      //drug doctypes just get orginal code
-            $xml = '<!DOCTYPE drug_guide PUBLIC "-//ES//DTD drug_guide DTD version 3.4//EN//XML" "https://major-tool-development.s3.amazonaws.com/DTDs/3_8_drug.dtd">' . "\n";
-            $xml .= '<drug_guide isbn="0000000000000">' . "\n";	//TODO: make the ISBN dynamic
-            $xml .= $molecule->export($statusId);
-            $xml .= '</drug_guide>';
-            $zip->addFromString($code .'_xml_'. date('Y-m-d_H:i:s') .'.xml', $xml);
-            $zip->close();
-        }
+					break;
+			}
+
+			$xml = $xmlDoctype . "\n";
+			$xml .= '<' . $xmlRoot . ' isbn="' . $productInfo['isbn'] . '">' . "\n";
+			$xml .= '<chapter id="c' . $code . '" number="' . $code . '">' . "\n";
+			$xml .= $moleculeXml;
+			$xml .= '</chapter>' . "\n";
+			$xml .= '</'. $xmlRoot . '>';
+			$zip->addFromString($code . '.xml', $xml);
+
+			//TODO: generate metaheader
+			//TODO: get figureLog
+			//TODO: consider whether addIllustrationLog should modify zip or not
+			//TODO: modify the next few lines
+			//TODO: copy this to dictionary doctype above
+			$figureLog = $molecule->addFigureLog($moleculeXml, $metaheader_default);
+			$zip->addFromString('IllustrationLog_' . $code . '.tsv' ,  $figureLog);
+			$this->addIllustrationLog($moleculeXml, $zip, $productInfo);
+			
+			$this->addIllustrations($moleculeXml, $zip, $productInfo);
+
+
+		//urecognized and drug doctypes just get orginal code; also skip illustrations for now
+		} else {
+			$xmlDoctype = '<!DOCTYPE drug_guide PUBLIC "-//ES//DTD drug_guide DTD version 3.4//EN//XML" "https://major-tool-development.s3.amazonaws.com/DTDs/3_8_drug.dtd">';
+			$xmlRoot = 'drug_guide';
+
+			$xml = $xmlDoctype . "\n";
+			$xml .= '<' . $xmlRoot . ' isbn="' . $productInfo['isbn'] . '">' . "\n";
+			$xml .= '<body>' . "\n";
+			$xml .= $moleculeXml;
+			$xml .= '</body>' . "\n";
+			$xml .= '</'. $xmlRoot . '>';
+			$zip->addFromString($code . '.xml', $xml);
+		}
+
+		$zip->close();
 
         header('Content-Type: application/zip');
         header('Content-Disposition: attachment; filename="' . $filename . '"');
@@ -181,5 +232,99 @@ METAHEADER;
                 ->countExportable($statusId);
 
         return new ApiPayload($count);
-    }
+	}
+	
+
+	/**
+	 * Add illustration files to the export
+	 *
+	 * @api
+	 *
+	 * @param string $moleculeXml The current molecule's XML in which the illustrations are to be found
+	 * @param object $zip The zip object into which to add the illustrations
+	 * @param array $productInfo Information about the current product
+	 *
+	 * @return boolean Also modifies provided $zip
+	 */
+	public function addIllustrations($moleculeXml, $zip, $productInfo) {
+		//TODO: pick these up from configuration
+		$s3UrlDev = 'https://s3.amazonaws.com/metis-imageserver-dev.elseviermultimedia.us';
+		$s3UrlProd = 'https://s3.amazonaws.com/metis-imageserver.elseviermultimedia.us';
+		//these must be in order of preference
+		$imageExtensions = ['eps', 'tif', 'jpg',];
+
+		$imageFiles = $molecule->getImageFileName($moleculeXml);
+		foreach($imageFiles as $imageFile) {
+			$imageFound = false;
+			
+			//suggested image
+			if(substr($imageFile, 0, 9) == 'suggested') {
+				$imageDir = '';
+
+			//legacy image
+			} else {
+				$imageDir = $productInfo['isbn'] . "/";
+			}
+
+			foreach($imageExtensions as $imageExtension) {
+				$imagePath = $s3UrlProd . "/" . $imageDir . $imageFile . "." . $imageExtension;
+				
+				//NOTE: we're ignoring errors because we _expect_ failures and want to quickly skip to next attempt
+				if(!$image = @file_get_contents($imagePath . $imageExtension)) {
+					if(!$image = @file_get_contents($imagePath . strtoupper($imageExtension))) {
+						//not found, check next extension
+						continue;
+					}
+					//found CAPITAL EXTENSION
+					$imageFound = true;
+					break;
+				}
+				//found default extension
+				$imageFound = true;
+				break;
+
+			}
+
+			if($imageFound === true && $image) {
+				$zip->addFromString(pathinfo(parse_url($imagePath, PHP_URL_PATH), PATHINFO_BASENAME), $image);
+
+			} else {
+				//TODO: log an error message because this was not found
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Add illustration log to the export
+	 *
+	 * @api
+	 *
+	 * @param string $moleculeXml The current molecule's XML in which the illustrations are to be found
+	 * @param object $zip The zip object into which to add the illustrations
+	 * @param array $productInfo Information about the current product
+	 *
+	 * @return boolean Also modifies provided $zip
+	 */
+	public function addIllustrationLog($moleculeXml, $zip, $productInfo) {
+		//Top Header material for tab delimited illustration log download. static content added to log.
+		$metaheader_default = <<<METAHEADER
+Illustration Processing Control Sheet\t\t\t\t\t\t\t\t\t\t\t
+Author:\t{$productInfo['author']}\t\t\t\t\t\t\t\t\t\t\tISBN:\t{$productInfo['isbn']}\t\t\t\t
+Title:\t{$productInfo['title']}\t\t\t\t\t\t\t\t\t\t\tEdition:\t{$productInfo['edition']}\t\t\t\t
+Processor:\t{$productInfo['cds']['firstname']} {$productInfo['cds']['lastname']}\t\t\t\t\t\t\t\t\t\t\tChapter:\t{$code}\t\t\t\t
+Phone/Email:\t{$productInfo['cds']['phone']}/{$productInfo['cds']['email']}\t\t\t\t\t\t\t\t\t\t\tDate:\t{$zipDate}\t\t\t\t
+Figure Number\tPieces (No.)\tDigital (Y/N)\tTo Come\t Previous edition fig #\t Borrowed from other Elsevier sources (author(s), title, ed, fig #)\tDigital file name (include disc number if multiple discs)\tFINAL FIG FILE NAME\t 1/C HT\t 2/C HT\t 4/C HT\t 1/C LD\t 2/C LD\t 4/C LD\tArt category\tArt point of contact\t Comments\n
+METAHEADER;
+
+		$figureLog = $molecule->addFigureLog($moleculeXml, $metaheader_default);
+		$zip->addFromString('IllustrationLog_' . $code . '.tsv' ,  $figureLog);
+
+		return true;
+	}
+
+
 }
