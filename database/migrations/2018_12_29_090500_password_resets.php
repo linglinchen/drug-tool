7ee426cb48aa0e4ec4f22e018b64c6f73dc969f4
Craig Russell <c.russell.2@elsevier.com>
2018-12-29 22:55:33 -0600

    Add password reset fields to the database

---
+++ database/migrations/2018_12_29_090500_password_resets.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PasswordResets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->string('reset_token')->nullable();
+            $table->timestamp('reset_token_expiration')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function($table) {
+            $table->dropColumn('reset_token');
+            $table->dropColumn('reset_token_expiration');
+        });
+    }
+}
