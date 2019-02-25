afaa44dbae903c178dcbd4123d70b540b5596877
Jonathan Schmidt <j.schmidt@elsevier.com>
2019-02-25 14:28:40 -0600

    book doctyp defined

--- app/BookDoctype.php
+++ app/BookDoctype.php
@@ -8,31 +8,22 @@ use App\Atom;
 
 class BookDoctype extends AbstractDoctype {
     protected $_config = [
-        'validAtomRootElements' => ['entry'],
-        'validTitleElements' => ['headw'],
+        'validAtomRootElements' => ['chapter'],
+        'ignoreAtomRootElements' => ['info', 'ce:label'],
+        'validTitleElements' => ['ce:title'],
         'idPrefixes' => [
-            'entry' => 'me'
+            'chapter' => 'c'
         ],
         'chapterElement' => [
-            'elementXpath' => '//alpha',
-            'keyAttributeName' => 'letter',
+            'elementXpath' => '//molecule',
+            'keyAttributeName' => 'code',
         ]
     ];
 
     public function beforeSave($atom) {
          $originalAtom = Atom::findNewestIfNotDeleted($atom->entity_id, $atom->product_id);
          if ($originalAtom){ // for existing atom
-            $originalDomainCode = $originalAtom->domain_code;
-            if($originalDomainCode != $atom->domain_code) {
-                $replacement = '$1' . $atom->domain_code . '$3';
-                $atom->xml = preg_replace('/(<category[^>]*>)(.*)(<\/category>)/Ssi', $replacement, $atom->xml);
-            }
-            else {
-                preg_match('/<category[^>]*>(.*)<\/category>/Si', $atom->xml, $matches);
-                if($matches) {
-                    $atom->domain_code = trim($matches[1]);
-                }
-            }
+
         } //if it's new atom, just return true
         return true;
     }
